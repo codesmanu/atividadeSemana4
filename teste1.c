5813cb74b12d3da85a8092b938feb06cf3706359
codesmanu <contact.rackel@gmail.com>
2025-04-14 09:41:16 -0300

    alterações nas variáveis

--- teste1.c
+++ teste1.c
@@ -9,35 +9,36 @@
 int main()
 {
     int idade[5];
-    int i, j, temp; // "temp" vai servir como uma variável auxiliar para trocar dois valores de posição no vetor durante a ordenação (temp - espaço temporário)
+    int x, z, box; // "box" vai servir como uma variável auxiliar para trocar dois valores de posição no vetor durante a ordenação (box - espaço temporário), como se fosse uma caixinha extra (por isso o box)
 
 
     // Ler as idades
-    for (i = 0; i < 5; i++)
+    for (x = 0; x < 5; x++)
     {
-        printf("escreva a idade %d: ", i + 1);
-        scanf("%d", &idade[i]); // %d especificador de formato que indica que o valor a ser impresso é um número inteiro decimal (do tipo int).
+        printf("escreva a idade %d: ", x + 1); 
+
+        scanf("%d", &idade[x]); // %d especificador de formato que indica que o valor a ser impresso é um número inteiro decimal (do tipo int).
     }
 
     // ordem das idades
-    for (i = 0; i < 4; i++) { // ler as numerações dadas até o penúltimo número dado
-        for (j = i + 1; j < 5; j++) { // comparação de ambos os números
-            if (idade[i] > idade[j]) { // bubble sort, compara dois valores de cada vez e troca de ordem se necessário
-                temp = idade[i]; // troca de nmr
-                idade[i] = idade[j]; // troca de nmr
-               idade[j] = temp; // troca de nmr
+    for (x = 0; x < 4; x++) { // ler as numerações dadas até o penúltimo número dado
+        for (z = x + 1; z < 5; z++) { // comparação de ambos os números
+            if (idade[x] > idade[z]) { // "bubble sort", compara dois valores de cada vez e troca de ordem se necessário
+                box = idade[x]; // troca de nmr
+                idade[x] = idade[z]; // troca de nmr
+                idade[z] = box; // troca de nmr
 
             }
         }        
     }
       
         //ordem crescente 
-    for (i = 0; i <= 4; i++) // "i <=" n pode ser 5, pois causaria erro de acesso fora do vetor (porque índices vão de 0 a 4)
+    for (x = 0; x <= 4; x++) // "i <=" n pode ser 5, pois causaria erro de acesso fora do vetor (porque índices vão de 0 a 4)
     {
-        printf("%d\n", idade[i]);
+        printf("%d\n", idade[x]);
     }
 
     return 0;
 }
 
-// gcc teste1.c -o teste1.exe; .\teste1.exe (colocar esse comando no terminal para executar o programa).
+// gcc teste1.c -o teste1.exe; .\teste1.exe (colocar esse comando no terminal para executar o programa, por causa do mingw2).
